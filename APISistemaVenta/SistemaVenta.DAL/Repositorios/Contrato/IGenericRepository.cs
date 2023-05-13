81f4cb4ff105c0a191dd799e7ee0f1db13abef3f
Francisco Ruiz <fruiz@persianasclassic.com>
2023-05-12 18:36:39 -0600

    Se configura la interfaz y el repositorio generico para acciones a la base de datos

---
+++ APISistemaVenta/SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(Expression<Func<T, bool>> filtro);
+        Task<T> Create(T modelo);
+        Task<bool> Update(T modelo);
+        Task<bool> Delete(T modelo);
+        Task<IQueryable<T>> Query(Expression<Func<T>> filtro = null);
+    }
+}
---
+++ APISistemaVenta/SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbventaContext _dbContext;
+        public GenericRepository(DbventaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> Create(T modelo)
+        {
+            try
+            {
+                _dbContext.Set<T>().Add(modelo);
+                await _dbContext.SaveChangesAsync();
+                return modelo;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Delete(T modelo)
+        {
+            try
+            {
+                _dbContext.Set<T>().Remove(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> filtro)
+        {
+            try
+            {
+                T modelo = await _dbContext.Set<T>().FirstOrDefaultAsync(filtro);
+                return modelo;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IQueryable<T>> Query(Expression<Func<T>> filtro = null)
+        {
+            try
+            {
+                IQueryable<T> query = filtro == null ? _dbContext.Set<T>() : _dbContext.Set<T>().Where(filtro);
+                return query;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Update(T modelo)
+        {
+            try
+            {
+                _dbContext.Set<T>().Update(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
