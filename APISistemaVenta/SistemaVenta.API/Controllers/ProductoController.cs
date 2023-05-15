1654bfc2eaa61534ab6710d221657f2efbb93f6c
Francisco Ruiz <fruiz_cruz@outlook.com>
2023-05-14 20:17:22 -0600

    Se crean los controllers, y se configura CORS

---
+++ APISistemaVenta/SistemaVenta.API/Controllers/ProductoController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoService _productoService;
+
+        public ProductoController(IProductoService productoService)
+        {
+            _productoService = productoService;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var response = new Response<List<ProductoDTO>>();
+            try
+            {
+                response.status = true;
+                response.value = await _productoService.Lista();
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.msg = ex.Message;
+            }
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Crear")]
+        public async Task<IActionResult> Crear([FromBody] ProductoDTO producto)
+        {
+            var response = new Response<ProductoDTO>();
+            try
+            {
+                response.status = true;
+                response.value = await _productoService.Crear(producto);
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.msg = ex.Message;
+            }
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] ProductoDTO producto)
+        {
+            var response = new Response<bool>();
+            try
+            {
+                response.status = true;
+                response.value = await _productoService.Editar(producto);
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.msg = ex.Message;
+            }
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar(id int")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var response = new Response<bool>();
+            try
+            {
+                response.status = true;
+                response.value = await _productoService.Eliminar(id);
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.msg = ex.Message;
+            }
+            return Ok(response);
+        }
+    }
+}
